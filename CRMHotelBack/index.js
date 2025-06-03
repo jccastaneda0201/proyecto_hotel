abed951ec1f624a925fdf2bbf85e5f1711b2f936
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-06-03 16:57:37 +0200

    Update index.js

--- CRMHotelBack/index.js
+++ CRMHotelBack/index.js
@@ -1,9 +1,9 @@
 // Server creation and configuration
-const http = require('http');
-const app = require('./src/app');
+const http = require("http");
+const app = require("./src/app");
 
 // Config .env
-require('dotenv').config();
+require("dotenv").config();
 
 // Server creation
 const server = http.createServer(app);
@@ -12,10 +12,10 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
 // Listeners
-server.on('listening', () => {
-    console.log(`Server listening on port ${PORT}`);
+server.on("listening", () => {
+  console.log(`Server listening on port ${PORT}, conectado a DB`);
 });
 
-server.on('error', (error) => {
-    console.log(error);
+server.on("error", (error) => {
+  console.log(error);
 });
